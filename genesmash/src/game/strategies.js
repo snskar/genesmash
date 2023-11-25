04b2053ca7b843eb30bffe846e3183c2c51f4b26
snskar <s4nskar@gmail.com>
2023-11-25 12:34:00 +0530

    installed prettier and modified strategies file

--- genesmash/src/game/strategies.js
+++ genesmash/src/game/strategies.js
@@ -1,34 +1,34 @@
-class Cooperate {
-    
-    static name = 'Cooperate';
-    static description = 'Player always cooperates';
-    chooseAction(playerActions, opponentActions) {
-        return 'cooperate';
-    }
-}
+module.exports.strategies = {
 
-class Defect {
-    
-    static name = 'Defect';
-    static description = 'Player always defects';
-    chooseAction(playerActions, opponentActions) {
-        return 'defect';
-    }
-}
-
-class TitForTat {
-    static name = 'Tit for Tat';
-    static description = 'A simple strategy in which the player always plays the last move of the opponent, rewarding nice players and retaliating against cheaters';
-    chooseAction(playerActions, opponentActions) {
-        if(opponentActions && opponentActions.length < 1){
+    Cooperate : {
+        
+        id: 1,
+        name : 'Cooperate',
+        description : 'Player always cooperates',
+        chooseAction : function(playerActions, opponentActions) {
             return 'cooperate';
-        }
-        return opponentActions && opponentActions[opponentActions.length -1];
-    }
-}
+        },
+    },
 
-module.exports = {
-    Cooperate,
-    Defect,
-    TitForTat,
-};
+    Defect : {
+        
+        id : 2,
+        name : 'Defect',
+        description : 'Player always defects',
+        chooseAction : function(playerActions, opponentActions) {
+            return 'defect';
+        },
+    },
+
+     TitForTat :  {
+        id : 3,
+        name : 'Tit-for-Tat',
+        description : 'A simple strategy in which the player always plays the last move of the opponent, rewarding nice players and retaliating against cheaters',
+        chooseAction : function(playerActions, opponentActions) {
+            if(opponentActions && opponentActions.length < 1){
+                return 'cooperate';
+            }
+            return opponentActions && opponentActions[opponentActions.length -1];
+        },
+    },   
+}
