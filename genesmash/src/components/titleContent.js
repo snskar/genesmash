0ac781227089de3e7558ba54d7ce289d6af405b0
snskar <s4nskar@gmail.com>
2023-12-01 00:41:37 +0530

    some more file restructuring

---
+++ genesmash/src/components/titleContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/titleContent.css'; // Create a CSS file for styling
+
+const TitleContent = () => {
+  return (
+    <div className='title-subtitle-container'>
+      <h2 className='upper-subtitle'>ITERATED PRISONERS DILEMMA</h2>
+      <h1 className='title'>GENE SMASH</h1>
+      <h2 className='lower-subtitle'>A VISUALISATION OF EVOLUTION OF BEHAVIOUR</h2>
+    </div>
+  );
+};
+
+export default TitleContent;
